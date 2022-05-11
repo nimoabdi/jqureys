579fc429736ec262d0c79d6f04cff7a40b4813c1
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-11 16:19:52 -0500

    manipulating dom

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ function onReady(){
     console.log('titleHeader', titleHeader)
     //let titleHeader = <h1>..</h1>
 
+
+
+    let allListItems = $('li');
+    console.log('li elements', allListItems);
+
+    // id
+
+    let justOneListItem = $('#madam-li-element');
+    justOneListItem.text('DOM Manipulation')
+
+    //class
+
+    $('.first-and-last').css('color', 'blue')
+
+    $('ul').append('<li>forms</li>')
+
+
 }
