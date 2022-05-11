355e0f7a39d1bfa546fe70634dfe971a5864f906
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-11 15:54:49 -0500

    created jquery and script

---
+++ README.md
@@ -0,0 +1,10 @@
+#Intro to Jquery 
+
+things to learn
+
+- selectors
+- events
+- getters and setters
+- events
+
+DOM = document object model
---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('In script.js');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('ready yea');
+
+    
+    let titleHeader = $('h1');
+    console.log('titleHeader', titleHeader)
+    //let titleHeader = <h1>..</h1>
+
+}
